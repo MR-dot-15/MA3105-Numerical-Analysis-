97ea3e36ad32b5953ebc2a1454ed467ff69f1bf6
MR-dot-15 <76465734+MR-dot-15@users.noreply.github.com>
2021-11-22 03:38:36 +0530

    Add files via upload

---
+++ Day1/assign2.m
@@ -0,0 +1,9 @@
+x = randperm(20);
+% creating repetition
+x(3) = x(1); x(13) = x(8); x(17) = x(11);
+disp(x);
+for i = 1:length(x)
+  if sum(x == x(i)) > 1
+    disp(["Duplicates: ", num2str(x(i))]);
+  endif
+endfor
